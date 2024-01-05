113fae6d6b66c2913947d8b7fec3e218ab71b29a
Lie Yan <robyn.lie.yan@outlook.com>
2024-01-05 21:59:11 +0800

    feat(math kern): implement math kern (#14)

--- Sources/SwiftOpenType/MATH/MathGlyphInfoTable.swift
+++ Sources/SwiftOpenType/MATH/MathGlyphInfoTable.swift
@@ -25,12 +25,12 @@ public class MathGlyphInfoTable {
     /// When the glyph to the left or right of a box is an extended shape variant, the (ink) box
     /// should be used for vertical positioning purposes, not the default position defined by
     /// values in MathConstants table. May be NULL.
-    var extendedShapeCoverageOffset: Offset16 {
+    public func extendedShapeCoverageOffset() -> Offset16 {
         data.readOffset16(parentOffset: tableOffset, offset: 4)
     }
 
     /// Offset to MathKernInfo table, from the beginning of the MathGlyphInfo table.
-    var mathKernInfoOffset: Offset16 {
+    public func mathKernInfoOffset() -> Offset16 {
         data.readOffset16(parentOffset: tableOffset, offset: 6)
     }
 
@@ -45,11 +45,33 @@ public class MathGlyphInfoTable {
         return nil
     }
     
+    /// The MathTopAccentAttachment table contains information on horizontal positioning of top math accents.
     public var mathTopAccentAttachmentTable: MathTopAccentAttachmentTable? {
         let subtableOffset = mathTopAccentAttachmentOffset()
         
         if subtableOffset != 0 {
             return MathTopAccentAttachmentTable(data: data, tableOffset: self.tableOffset + subtableOffset)
+        }
+        return nil
+    }
+    
+    /// The glyphs covered by this table are to be considered extended shapes.
+    public var extendedShapeCoverageTable: CoverageTable? {
+        let subtableOffset = extendedShapeCoverageOffset()
+        
+        if subtableOffset != 0 {
+            return CoverageTable(data: data, tableOffset: self.tableOffset + subtableOffset)
+        }
+        return nil
+    }
+    
+    /// The MathKernInfo table provides mathematical kerning values used for kerning
+    /// of subscript and superscript glyphs relative to a base glyph.
+    public var mathKernInfoTable: MathKernInfoTable? {
+        let subtableOffset = mathKernInfoOffset()
+        
+        if subtableOffset != 0 {
+            return MathKernInfoTable(data: data, tableOffset: self.tableOffset + subtableOffset)
         }
         return nil
     }
@@ -84,7 +106,7 @@ public class MathItalicsCorrectionInfoTable {
     }
 
     /// Return italics correction for glyphID in design units
-    public func getItalicsCorrection(_ glyphID: UInt16) -> Int32 {
+    public func getItalicsCorrection(_ glyphID: UInt16) -> Int32? {
         let coverageTable = self.coverageTable()
         if let coverageIndex = coverageTable.getCoverageIndex(glyphID) {
             let mathValueRecord = italicsCorrection(coverageIndex)
@@ -92,7 +114,7 @@ public class MathItalicsCorrectionInfoTable {
                                                  mathValueRecord: mathValueRecord)
             return value
         }
-        return 0
+        return nil
     }
 }
 
@@ -126,7 +148,7 @@ public class MathTopAccentAttachmentTable {
     }
 
     /// Return top accent attachment for glyphID in design units
-    public func getTopAccentAttachment(_ glyphID: UInt16) -> Int32 {
+    public func getTopAccentAttachment(_ glyphID: UInt16) -> Int32? {
         let coverageTable = self.coverageTable()
         if let coverageIndex = coverageTable.getCoverageIndex(glyphID) {
             let mathValueRecord = topAccentAttachment(coverageIndex)
@@ -134,6 +156,244 @@ public class MathTopAccentAttachmentTable {
                                                  mathValueRecord: mathValueRecord)
             return value
         }
-        return 0
+        return nil
+    }
+}
+
+public class MathKernInfoTable {
+    let data: CFData
+    let tableOffset: Offset16
+    
+    init(data: CFData, tableOffset: Offset16) {
+        self.data = data
+        self.tableOffset = tableOffset
+    }
+    
+    // MARK: - table fields
+    
+    /// Offset to Coverage table, from the beginning of the MathKernInfo table.
+    public func mathKernCoverageOffset() -> Offset16 {
+        data.readOffset16(parentOffset: tableOffset, offset: 0)
+    }
+    
+    /// Number of MathKernInfoRecords. Must be the same as the number of glyph
+    /// IDs referenced in the Coverage table.
+    public func mathKernCount() -> UInt16 {
+        data.readUInt16(parentOffset: tableOffset, offset: 2)
+    }
+    
+    /// Array of MathKernInfoRecords, one for each covered glyph.
+    public func mathKernInfoRecords(_ index: Int) -> MathKernInfoRecord {
+        data.readMathKernInfoRecord(parentOffset: tableOffset, offset: 4 + index * MathKernInfoRecord.byteSize)
+    }
+    
+    // MARK: - optimization
+    
+    private func mathKernOffset(index: Int, corner: MathKernCorner) -> Offset16 {
+        let offset = 4 + index * MathKernInfoRecord.byteSize + corner.getByteOffset()
+        return data.readOffset16(parentOffset: tableOffset, offset: offset)
+    }
+    
+    private func getMathKernOffset(glyphID: UInt16, corner: MathKernCorner) -> Offset16? {
+        let coverageTable = self.coverageTable()
+        if let coverageIndex = coverageTable.getCoverageIndex(glyphID) {
+            return mathKernOffset(index: coverageIndex, corner: corner)
+        }
+        return nil
+    }
+    
+    // MARK: - query functions
+    
+    public func coverageTable() -> CoverageTable {
+        CoverageTable(data: data, tableOffset: tableOffset + mathKernCoverageOffset())
+    }
+    
+    public func getMathKernInfoRecord(_ glyphID: UInt16) -> MathKernInfoRecord? {
+        let coverageTable = self.coverageTable()
+        if let coverageIndex = coverageTable.getCoverageIndex(glyphID) {
+            return mathKernInfoRecords(coverageIndex)
+        }
+        return nil
+    }
+    
+    public func getKernValue(glyphID: UInt16, corner: MathKernCorner, height: Int32) -> Int32? {
+        if let mathKernOffset = getMathKernOffset(glyphID: glyphID, corner: corner) {
+            let mathKernTable = MathKernTable(data: data, tableOffset: self.tableOffset + mathKernOffset)
+            return mathKernTable.getKernValue(height: height)
+        }
+        return nil
+    }
+}
+
+public class MathKernTable {
+    let data: CFData
+    let tableOffset: Offset16
+    
+    init(data: CFData, tableOffset: Offset16) {
+        self.data = data
+        self.tableOffset = tableOffset
+    }
+    
+    // MARK: - table fields
+
+    /// Number of heights at which the kern value changes.
+    public func heightCount() -> UInt16 {
+        data.readUInt16(parentOffset: tableOffset, offset: 0)
+    }
+    
+    /// Array of correction heights, in design units, sorted from lowest to highest.
+    public func correctionHeight(_ index: Int) -> MathValueRecord {
+        data.readMathValueRecord(parentOffset: tableOffset, offset: 2 + index * MathValueRecord.byteSize)
+    }
+    
+    /// Array of kerning values for different height ranges.
+    /// Negative values are used to move glyphs closer to each other.
+    public func kernValues(_ index: Int) -> MathValueRecord {
+        let offset = 2 + Int(heightCount()) * MathValueRecord.byteSize + index * MathValueRecord.byteSize
+        return data.readMathValueRecord(parentOffset: tableOffset, offset: offset)
+    }
+    
+    // MARK: - query functions
+    
+    /// Return the correction height at the given index in design units
+    public func getCorrectionHeight(_ index: Int) -> Int32 {
+        let mathValueRecord = self.correctionHeight(index)
+        let value = data.evalMathValueRecord(parentOffset: tableOffset, mathValueRecord: mathValueRecord)
+        return value
+    }
+    
+    /// Return the kern value at the given index in design units
+    public func getKernValue(_ index: Int) -> Int32 {
+        let mathValueRecord = self.kernValues(index)
+        let value = data.evalMathValueRecord(parentOffset: tableOffset, mathValueRecord: mathValueRecord)
+        return value
+    }
+    
+    /// Return the kern value for the given height in design units
+    public func getKernValue(height: Int32) -> Int32 {
+        if let index = upper_bound(height: height) {
+            return self.getKernValue(index)
+        }
+        return self.getKernValue(0)
+    }
+    
+    // MARK: - helper functions
+
+    /// Return the index of the first element not less than the given height.
+    /// We borrow the implementation of `std::lower_bound()` from C++ STL.
+    private func lower_bound(height: Int32) -> Int? {
+        var count = Int(self.heightCount())
+        var first = 0
+        let last = count
+        
+        while (count > 0) {
+            var it = first
+            let step = count / 2
+            it += step
+            
+            if (getCorrectionHeight(it) < height) {
+                it += 1
+                first = it
+                count -= step + 1
+            }
+            else {
+                count = step
+            }
+        }
+        return first == last ? nil : first
+    }
+    
+    /// Return the index of the first element greater than the given height.
+    /// We borrow the implementation of `std::upper_bound()` from C++ STL.
+    private func upper_bound(height: Int32) -> Int? {
+        var count = Int(self.heightCount())
+        var first = 0
+        let last = count
+        
+        while (count > 0) {
+            var it = first
+            let step = count / 2
+            it += step
+            
+            if !(height < getCorrectionHeight(it)) {
+                it += 1
+                first = it
+                count -= step + 1
+            }
+            else {
+                count = step
+            }
+        }
+        
+        if first != last {
+            return first
+        }
+        else if height < getCorrectionHeight(0) {
+            return nil
+        }
+        else {
+            return last
+        }
+    }
+}
+
+public enum MathKernCorner : Int {
+    case TopRight = 0
+    case TopLeft = 1
+    case BottomRight = 2
+    case BottomLeft = 3
+    
+    func getByteOffset() -> Int {
+        rawValue * 2
+    }
+}
+
+public struct MathKernInfoRecord {
+    static let byteSize = 8
+    
+    /// Offset to MathKern table for top right corner, from the beginning
+    /// of the MathKernInfo table. May be NULL.
+    public let topRightMathKernOffset: Offset16
+    
+    /// Offset to MathKern table for the top left corner, from the beginning
+    /// of the MathKernInfo table. May be NULL.
+    public let topLeftMathKernOffset: Offset16
+    
+    /// Offset to MathKern table for bottom right corner, from the beginning
+    /// of the MathKernInfo table. May be NULL.
+    public let bottomRightMathKernOffset: Offset16
+    
+    /// Offset to MathKern table for bottom left corner, from the beginning
+    /// of the MathKernInfo table. May be NULL.
+    public let bottomLeftMathKernOffset: Offset16
+    
+    init() {
+        self.init(topRightMathKernOffset: 0,
+                  topLeftMathKernOffset: 0,
+                  bottomRightMathKernOffset: 0,
+                  bottomLeftMathKernOffset: 0)
+    }
+    
+    init(topRightMathKernOffset: Offset16,
+         topLeftMathKernOffset: Offset16,
+         bottomRightMathKernOffset: Offset16,
+         bottomLeftMathKernOffset: Offset16) {
+        self.topRightMathKernOffset = topRightMathKernOffset
+        self.topLeftMathKernOffset = topLeftMathKernOffset
+        self.bottomRightMathKernOffset = bottomRightMathKernOffset
+        self.bottomLeftMathKernOffset = bottomLeftMathKernOffset
+    }
+    
+    func getMathKernOffset(corner: MathKernCorner) -> Offset16 {
+        switch corner {
+        case .TopRight:
+            return topRightMathKernOffset
+        case .TopLeft:
+            return topLeftMathKernOffset
+        case .BottomRight:
+            return bottomRightMathKernOffset
+        case .BottomLeft:
+            return bottomLeftMathKernOffset
+        }
     }
 }
--- Tests/SwiftOpenTypeTests/MATH/MathTableTests.swift
+++ Tests/SwiftOpenTypeTests/MATH/MathTableTests.swift
@@ -32,15 +32,12 @@ final class MathTableTests: XCTestCase {
 
         XCTAssertEqual(toRatio(table.scriptPercentScaleDown), 0.7)
         XCTAssertEqual(toRatio(table.scriptScriptPercentScaleDown), 0.5)
-
         XCTAssertEqual(toPoints(table.delimitedSubFormulaMinHeight), 15.6)
         XCTAssertEqual(toPoints(table.displayOperatorMinHeight), 15.6)
-
         XCTAssertEqual(toPoints(table.mathLeading), 1.848)
         XCTAssertEqual(toPoints(table.axisHeight), 3.0)
         XCTAssertEqual(toPoints(table.accentBaseHeight), 5.4)
         XCTAssertEqual(toPoints(table.flattenedAccentBaseHeight), 7.968)
-
         XCTAssertEqual(toPoints(table.subscriptShiftDown), 2.964)
         XCTAssertEqual(toPoints(table.subscriptTopMax), 4.128)
         XCTAssertEqual(toPoints(table.subscriptBaselineDropMin), 2.4)
@@ -51,24 +48,20 @@ final class MathTableTests: XCTestCase {
         XCTAssertEqual(toPoints(table.subSuperscriptGapMin), 1.92)
         XCTAssertEqual(toPoints(table.superscriptBottomMaxWithSubscript), 4.128)
         XCTAssertEqual(toPoints(table.spaceAfterScript), 0.672)
-
         XCTAssertEqual(toPoints(table.upperLimitGapMin), 2.4)
         XCTAssertEqual(toPoints(table.upperLimitBaselineRiseMin), 1.332)
         XCTAssertEqual(toPoints(table.lowerLimitGapMin), 2.004)
         XCTAssertEqual(toPoints(table.lowerLimitBaselineDropMin), 7.2)
-
         XCTAssertEqual(toPoints(table.stackTopShiftUp), 5.328)
         XCTAssertEqual(toPoints(table.stackTopDisplayStyleShiftUp), 8.124)
         XCTAssertEqual(toPoints(table.stackBottomShiftDown), 4.14)
         XCTAssertEqual(toPoints(table.stackBottomDisplayStyleShiftDown), 8.232)
         XCTAssertEqual(toPoints(table.stackGapMin), 1.44)
         XCTAssertEqual(toPoints(table.stackDisplayStyleGapMin), 3.36)
-
         XCTAssertEqual(toPoints(table.stretchStackTopShiftUp), 1.332)
         XCTAssertEqual(toPoints(table.stretchStackBottomShiftDown), 7.2)
         XCTAssertEqual(toPoints(table.stretchStackGapAboveMin), 2.4)
         XCTAssertEqual(toPoints(table.stretchStackGapBelowMin), 2.004)
-
         XCTAssertEqual(toPoints(table.fractionNumeratorShiftUp), 4.728)
         XCTAssertEqual(toPoints(table.fractionNumeratorDisplayStyleShiftUp), 8.124)
         XCTAssertEqual(toPoints(table.fractionDenominatorShiftDown), 4.14)
@@ -78,25 +71,20 @@ final class MathTableTests: XCTestCase {
         XCTAssertEqual(toPoints(table.fractionRuleThickness), ruleThickness)
         XCTAssertEqual(toPoints(table.fractionDenominatorGapMin), ruleThickness)
         XCTAssertEqual(toPoints(table.fractionDenomDisplayStyleGapMin), commonGap)
-
         XCTAssertEqual(toPoints(table.skewedFractionHorizontalGap), 4.2)
         XCTAssertEqual(toPoints(table.skewedFractionVerticalGap), 1.152, accuracy: eps)
-
         XCTAssertEqual(toPoints(table.overbarVerticalGap), commonGap)
         XCTAssertEqual(toPoints(table.overbarRuleThickness), ruleThickness)
         XCTAssertEqual(toPoints(table.overbarExtraAscender), ruleThickness)
-
         XCTAssertEqual(toPoints(table.underbarVerticalGap), commonGap)
         XCTAssertEqual(toPoints(table.underbarRuleThickness), ruleThickness)
         XCTAssertEqual(toPoints(table.underbarExtraDescender), ruleThickness)
-
         XCTAssertEqual(toPoints(table.radicalVerticalGap), 0.6)
         XCTAssertEqual(toPoints(table.radicalDisplayStyleVerticalGap), 1.776)
         XCTAssertEqual(toPoints(table.radicalRuleThickness), ruleThickness)
         XCTAssertEqual(toPoints(table.radicalExtraAscender), ruleThickness)
         XCTAssertEqual(toPoints(table.radicalKernBeforeDegree), 3.336)
         XCTAssertEqual(toPoints(table.radicalKernAfterDegree), -6.672)
-
         XCTAssertEqual(toRatio(table.radicalDegreeBottomRaisePercent), 0.6)
     }
 
@@ -119,6 +107,68 @@ final class MathTableTests: XCTestCase {
         glyph = CTFontGetGlyphWithName(font, "f" as CFString)
         XCTAssertEqual(table.getTopAccentAttachment(glyph), 262)
     }
+    
+    func testMathConstants_2() {
+        let font = openFont(path: "fonts/MathTestFontFull.otf", size: 10.0)
+        let table = font.mathTable!.mathConstantsTable!
+
+        XCTAssertEqual(table.scriptPercentScaleDown, 87)
+        XCTAssertEqual(table.scriptScriptPercentScaleDown, 76)
+        XCTAssertEqual(table.delimitedSubFormulaMinHeight, 100)
+        XCTAssertEqual(table.displayOperatorMinHeight, 200)
+        XCTAssertEqual(table.mathLeading, 300)
+        XCTAssertEqual(table.axisHeight, 400)
+        XCTAssertEqual(table.accentBaseHeight, 500)
+        XCTAssertEqual(table.flattenedAccentBaseHeight, 600)
+        XCTAssertEqual(table.subscriptShiftDown, 700)
+        XCTAssertEqual(table.subscriptTopMax, 800)
+        XCTAssertEqual(table.subscriptBaselineDropMin, 900)
+        XCTAssertEqual(table.superscriptShiftUp, 1100)
+        XCTAssertEqual(table.superscriptShiftUpCramped, 1200)
+        XCTAssertEqual(table.superscriptBottomMin, 1300)
+        XCTAssertEqual(table.superscriptBaselineDropMax, 1400)
+        XCTAssertEqual(table.subSuperscriptGapMin, 1500)
+        XCTAssertEqual(table.superscriptBottomMaxWithSubscript, 1600)
+        XCTAssertEqual(table.spaceAfterScript, 1700)
+        XCTAssertEqual(table.upperLimitGapMin, 1800)
+        XCTAssertEqual(table.upperLimitBaselineRiseMin, 1900)
+        XCTAssertEqual(table.lowerLimitGapMin, 2200)
+        XCTAssertEqual(table.lowerLimitBaselineDropMin, 2300)
+        XCTAssertEqual(table.stackTopShiftUp, 2400)
+        XCTAssertEqual(table.stackTopDisplayStyleShiftUp, 2500)
+        XCTAssertEqual(table.stackBottomShiftDown, 2600)
+        XCTAssertEqual(table.stackBottomDisplayStyleShiftDown, 2700)
+        XCTAssertEqual(table.stackGapMin, 2800)
+        XCTAssertEqual(table.stackDisplayStyleGapMin, 2900)
+        XCTAssertEqual(table.stretchStackTopShiftUp, 3000)
+        XCTAssertEqual(table.stretchStackBottomShiftDown, 3100)
+        XCTAssertEqual(table.stretchStackGapAboveMin, 3200)
+        XCTAssertEqual(table.stretchStackGapBelowMin, 3300)
+        XCTAssertEqual(table.fractionNumeratorShiftUp, 3400)
+        XCTAssertEqual(table.fractionNumeratorDisplayStyleShiftUp, 3500)
+        XCTAssertEqual(table.fractionDenominatorShiftDown, 3600)
+        XCTAssertEqual(table.fractionDenominatorDisplayStyleShiftDown, 3700)
+        XCTAssertEqual(table.fractionNumeratorGapMin, 3800)
+        XCTAssertEqual(table.fractionNumDisplayStyleGapMin, 3900)
+        XCTAssertEqual(table.fractionRuleThickness, 4000)
+        XCTAssertEqual(table.fractionDenominatorGapMin, 4100)
+        XCTAssertEqual(table.fractionDenomDisplayStyleGapMin, 4200)
+        XCTAssertEqual(table.skewedFractionHorizontalGap, 4300)
+        XCTAssertEqual(table.skewedFractionVerticalGap, 4400)
+        XCTAssertEqual(table.overbarVerticalGap, 4500)
+        XCTAssertEqual(table.overbarRuleThickness, 4600)
+        XCTAssertEqual(table.overbarExtraAscender, 4700)
+        XCTAssertEqual(table.underbarVerticalGap, 4800)
+        XCTAssertEqual(table.underbarRuleThickness, 4900)
+        XCTAssertEqual(table.underbarExtraDescender, 5000)
+        XCTAssertEqual(table.radicalVerticalGap, 5100)
+        XCTAssertEqual(table.radicalDisplayStyleVerticalGap, 5200)
+        XCTAssertEqual(table.radicalRuleThickness, 5300)
+        XCTAssertEqual(table.radicalExtraAscender, 5400)
+        XCTAssertEqual(table.radicalKernBeforeDegree, 5500)
+        XCTAssertEqual(table.radicalKernAfterDegree, 5600)
+        XCTAssertEqual(table.radicalDegreeBottomRaisePercent, 65)
+    }
 
     func testMathItalicsCorrection_2() {
         let font = openFont(path: "fonts/MathTestFontFull.otf", size: 10.0)
@@ -127,7 +177,7 @@ final class MathTableTests: XCTestCase {
         var glyph: CGGlyph
 
         glyph = CTFontGetGlyphWithName(font, "space" as CFString)
-        XCTAssertEqual(table.getItalicsCorrection(glyph), 0)
+        XCTAssertEqual(table.getItalicsCorrection(glyph), nil)
 
         glyph = CTFontGetGlyphWithName(font, "A" as CFString)
         XCTAssertEqual(table.getItalicsCorrection(glyph), 197)
@@ -138,7 +188,66 @@ final class MathTableTests: XCTestCase {
         glyph = CTFontGetGlyphWithName(font, "C" as CFString)
         XCTAssertEqual(table.getItalicsCorrection(glyph), 452)
     }
+    
+    func testMathTopAccentAttachment_2() {
+        let font = openFont(path: "fonts/MathTestFontFull.otf", size: 10.0)
+        let table = font.mathTable!.mathGlyphInfoTable!.mathTopAccentAttachmentTable!
         
+        var glyph: CGGlyph
+
+        glyph = CTFontGetGlyphWithName(font, "space" as CFString)
+        XCTAssertEqual(table.getTopAccentAttachment(glyph), nil)
+        
+        var advance: CGSize = CGSize()
+        CTFontGetAdvancesForGlyphs(font, CTFontOrientation.default, &glyph, &advance, 1)
+        let topAccentAttachment = advance.width / font.sizePerUnit() * 0.5
+        XCTAssertEqual(topAccentAttachment, 500)
+
+        glyph = CTFontGetGlyphWithName(font, "D" as CFString)
+        XCTAssertEqual(table.getTopAccentAttachment(glyph), 374)
+
+        glyph = CTFontGetGlyphWithName(font, "E" as CFString)
+        XCTAssertEqual(table.getTopAccentAttachment(glyph), 346)
+
+        glyph = CTFontGetGlyphWithName(font, "F" as CFString)
+        XCTAssertEqual(table.getTopAccentAttachment(glyph), 318)
+    }
+    
+    func testMathExtendedShape_2() {
+        let font = openFont(path: "fonts/MathTestFontFull.otf", size: 10.0)
+        let table = font.mathTable!.mathGlyphInfoTable!.extendedShapeCoverageTable
+        
+        var glyph: CGGlyph
+        
+        glyph = CTFontGetGlyphWithName(font, "G" as CFString)
+        XCTAssert(table?.getCoverageIndex(glyph) == nil)
+        
+        glyph = CTFontGetGlyphWithName(font, "H" as CFString)
+        XCTAssert(table?.getCoverageIndex(glyph) != nil)
+    }
+    
+    func testMathKernInfo_2() {
+        let font = openFont(path: "fonts/MathTestFontFull.otf", size: 10.0)
+        let table = font.mathTable!.mathGlyphInfoTable!.mathKernInfoTable!
+        
+        let glyph = CTFontGetGlyphWithName(font, "I" as CFString)
+        
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 7), 31) // less than min height
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 14), 52) // equal to min height
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 20), 52)
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 23), 73)
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 31), 73)
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 32), 94)
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 86), 220) // equal to max height
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 91), 220) // larger than max height
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 96), 220) // larger than max height
+
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopRight, height: 39), 94) // top right
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .TopLeft, height: 39), 55)  // top left
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .BottomRight, height: 39), 22) // bottom right
+        XCTAssertEqual(table.getKernValue(glyphID: glyph, corner: .BottomLeft, height: 39), 50) // bottom left
+    }
+    
     func openFont(path : String, size: CGFloat) -> CTFont {
         let resourcePath = Bundle.module.resourcePath!
         let path = resourcePath + "/" + path
